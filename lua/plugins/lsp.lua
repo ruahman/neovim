e11b05ac41e53b1560f96886aa78db094f8f4946
diego vila <ruahman@gmail.com>
2022-06-18 21:53:48 -0400

    python, ruby, rust
    added python ruby and rust to lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -7,41 +7,38 @@ local capabilities = require('cmp_nvim_lsp').update_capabilities(
   vim.lsp.protocol.make_client_capabilities()
   )
 
-lspconfig.ccls.setup{
-  capabilities,
-  root_dir = lspconfig.util.root_pattern("*.c"),
-}
--- -- python
--- lspconfig.pylsp.setup{
---   capabilities,
---   root_dir = lspconfig.util.root_pattern("*.toml","*.lock",".git","*.py"),
---   settings = {
---     pylsp = {
---       enable = true;
---       configurationSources = { "flake8" };
---       plugins = {
---         pycodestyle = { enabled = false; };
---         flake8 = { enabled = true; };
---         pyflakes = { enabled = false; };
---         pydocstyle = { enabled = true; };
---         yapf = { enabled = true; };
---       }
---     }
---   }
--- }
 
--- -- ruby
--- lspconfig.solargraph.setup{
---   capabilities,
---   root_dir = lspconfig.util.root_pattern("Gemfile",".git","*.rb"),
---   settings = {
---     solargraph = {
---       autoformat=true;
---       completion=true;
---       diagnostics=true;
---     }
---   }
--- }
+-- python
+lspconfig.pylsp.setup{
+  capabilities,
+  root_dir = lspconfig.util.root_pattern("*.toml","*.lock",".git","*.py"),
+  settings = {
+    pylsp = {
+      enable = true;
+      configurationSources = { "flake8" };
+      plugins = {
+        pycodestyle = { enabled = false; };
+        flake8 = { enabled = true; };
+        pyflakes = { enabled = false; };
+        pydocstyle = { enabled = true; };
+        yapf = { enabled = true; };
+      }
+    }
+  }
+}
+
+-- ruby
+lspconfig.solargraph.setup{
+  capabilities,
+  root_dir = lspconfig.util.root_pattern("Gemfile",".git","*.rb"),
+  settings = {
+    solargraph = {
+      autoformat=true;
+      completion=true;
+      diagnostics=true;
+    }
+  }
+}
 
 -- golang
 lspconfig.gopls.setup{
@@ -49,43 +46,44 @@ lspconfig.gopls.setup{
   root_dir = lspconfig.util.root_pattern("*.go","go.mod"),
 }
 
+
+-- rust
 local on_attach = function(client)
     require'completion'.on_attach(client)
 end
 
--- rust
 lspconfig.rust_analyzer.setup{
   capabilities,
   on_attach,
   settings = {
-        ["rust-analyzer"] = {
-            assist = {
-                importGranularity = "module",
-                importPrefix = "self",
-            },
-            cargo = {
-                loadOutDirsFromCheck = true
-            },
-            procMacro = {
-                enable = true
-            },
-        }
-    }
-  -- root_dir = lspconfig.util.root_pattern("Cargo.toml","*.rs"),
+      ["rust-analyzer"] = {
+          assist = {
+              importGranularity = "module",
+              importPrefix = "self",
+          },
+          cargo = {
+              loadOutDirsFromCheck = true
+          },
+          procMacro = {
+              enable = true
+          },
+      }
+  },
+  root_dir = lspconfig.util.root_pattern("Cargo.toml","*.rs"),
+}
+
+-- c,c++
+lspconfig.ccls.setup{
+  capabilities,
+  root_dir = lspconfig.util.root_pattern("*.c"),
 }
 
 -- key mappings
--- utils.map('n', 'gh', '<cmd>lua vim.lsp.buf.hover()<CR>')
 vim.keymap.set("n", "gh", vim.lsp.buf.hover) -- hover information
--- utils.map('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>')
 vim.keymap.set("n", "gd", vim.lsp.buf.definition) -- like definition of function
--- utils.map('n', 'gt', '<cmd>lua vim.lsp.buf.type_definition()<CR>')
 vim.keymap.set("n", "gt", vim.lsp.buf.type_definition) -- like definition of a type
--- utils.map('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>')
 vim.keymap.set("n", "gi", vim.lsp.buf.implementation) -- like implementaion of interface
--- utils.map('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>')
 vim.keymap.set("n", "gr", vim.lsp.buf.references)
--- utils.map('n', 'gf', '<cmd>lua vim.lsp.buf.formatting()<CR>')
 vim.keymap.set("n", "gf", vim.lsp.buf.formatting)
 
 -- auto-format
