ec839db55c2024c1b1ddf0fa123ad3edf150feea
diego vila <ruahman@gmail.com>
2022-07-22 21:50:21 -0400

    made Go commands

--- lua/commands/init.lua
+++ lua/commands/init.lua
@@ -1,1 +1,17 @@
-vim.cmd[[command! -nargs=+ Go :! go <args> .]]
+
+
+-- vim.cmd[[command! -nargs=+ Go :! go <args> .]]
+
+vim.api.nvim_create_user_command(
+  'Go',
+  function(input)
+    if string.match(input.args, "build") then
+      vim.cmd[[:! go build .]]
+    elseif string.match(input.args, "run") then
+      vim.cmd[[:! go run .]]
+    else
+      print(" go does not recognize command...")
+    end
+  end,
+  {nargs = 1}
+)
