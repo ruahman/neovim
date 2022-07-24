2e510830d673093c7e14db6075fcbc9a4c0c17a3
diego vila <ruahman@gmail.com>
2022-07-23 21:56:23 -0400

    added go check

--- lua/commands/init.lua
+++ lua/commands/init.lua
@@ -6,7 +6,9 @@ vim.api.nvim_create_user_command(
   'Go',
   function(input)
     if string.match(input.args, "build") then
-      vim.cmd[[:! go build .]]
+      vim.cmd[[:! go build]]
+    elseif string.match(input.args, "check") then
+      vim.cmd[[:! go build -o /dev/null]]
     elseif string.match(input.args, "run") then
       vim.cmd[[:! go run .]]
     else
