9518936ad9e28ad04e1ae920e2c69cc76a98c69f
Diego Vila <dego_vila@yahoo.com>
2022-11-27 04:52:23 +0000

    adde treesitter typescript and javascript

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
-  ensure_installed = { "c", "cpp", "lua", "rust", "go", "python", "ruby" },
+  ensure_installed = { "c", "cpp", "lua", "rust", "go", "python", "ruby", "typescript", "javascript" },
 
   -- highlight = {
   --   enable = true,
