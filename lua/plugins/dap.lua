db29d0ad52a8480946eaa6067d7c96467357fbd0
ruahman <ruahman@pop-os.localdomain>
2022-12-03 19:38:20 -0400

    got dap working

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -1,97 +1,88 @@
-local dap = require('dap')
+local export = {}
 
--- dap.set_log_level('TRACE')
+function export.config()
+  local dap = require("dap")
 
--- setup lldb for c/c++/rust
-dap.adapters.lldb = {
-  type = 'executable',
-  command = '/usr/bin/lldb-vscode', -- comes with install of lldb
-  name = 'lldb'
-}
+  dap.set_log_level("TRACE")
 
+  -- setup lldb for c/c++/rust
+  dap.adapters.lldb = {
+    type = "executable",
+    command = "/usr/bin/lldb-vscode-14", -- comes with install of lldb
+    name = "lldb",
+  }
 
---dap c
-dap.configurations.c = {
-  {
-    name = 'Launch',
-    type = 'lldb',
-    request = 'launch',
-    program = function()
-      -- load the debug file that was created using (clang -g )
-      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-    end,
-    cwd = '${workspaceFolder}',
-    stopOnEntry = false,
-    args = {},
-  },
-}
-
---dap cpp
-dap.configurations.cpp = {
-  {
-    name = 'Launch',
-    type = 'lldb',
-    request = 'launch',
-    program = function()
-      -- load the debug file that was created using (clang -g )
-      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-    end,
-    cwd = '${workspaceFolder}',
-    stopOnEntry = false,
-    args = {},
-  },
-}
-
---dap rust
-dap.configurations.rust = {
-  {
-    name = 'Launch',
-    type = 'lldb',
-    request = 'launch',
-    program = function()
-      -- load the debug file that was created using ( cargo build )
-      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/target/debug/', 'file')
-    end,
-    cwd = '${workspaceFolder}',
-    stopOnEntry = false,
-    args = {},
-  },
-}
-
---dap go
-require 'dap-go'.setup() -- must have delve installed globaly
-
---dap python
-require('dap-python').setup() -- must have debugpy installed globaly
-
-require 'nvim-dap-virtual-text'.setup()
-
---DAPUI
-require("dapui").setup({
-  layouts = {
+  -- c
+  dap.configurations.c = {
     {
-      elements = {
-        -- Elements can be strings or table with id and size keys.
-        { id = "scopes", size = 0.25 },
-        "watches",
-        "stacks",
-      },
-      size = 40, -- 40 columns
-      position = "left",
+      name = "Launch",
+      type = "lldb",
+      request = "launch",
+      program = function()
+        return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+      end,
+      cwd = "${workspaceFolder}",
+      stopOnEntry = false,
+      args = {},
     },
+  }
+
+  -- cpp
+  dap.configurations.cpp = dap.configurations.c
+
+  -- rust
+  dap.configurations.rust = {
     {
-      elements = {
-        "repl",
-      },
-      size = 0.25, -- 25% of total lines
-      position = "bottom",
+      name = "Launch",
+      type = "lldb",
+      request = "launch",
+      program = function()
+        return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/target/debug/", "file")
+      end,
+      cwd = "${workspaceFolder}",
+      stopOnEntry = false,
+      args = {},
     },
-  },
-})
+  }
 
+  --dap go
+  require("dap-go").setup() -- must have delve installed globaly
 
-vim.keymap.set('n', '<leader>b', require 'dap'.toggle_breakpoint)
-vim.keymap.set('n', '<leader>c', require 'dap'.continue)
-vim.keymap.set('n', '<leader>s', require 'dap'.step_into)
-vim.keymap.set('n', '<leader>n', require 'dap'.step_over)
-vim.keymap.set('n', '<leader>u', require 'dapui'.toggle)
+  --dap python
+  require("dap-python").setup() -- must have debugpy installed globaly
+
+  require("nvim-dap-virtual-text").setup()
+
+  --DAPUI
+  require("dapui").setup({
+    layouts = {
+      {
+        elements = {
+          -- Elements can be strings or table with id and size keys.
+          { id = "scopes", size = 0.25 },
+          "watches",
+          "stacks",
+        },
+        size = 40, -- 40 columns
+        position = "left",
+      },
+      {
+        elements = {
+          "repl",
+        },
+        size = 0.25, -- 25% of total lines
+        position = "bottom",
+      },
+    },
+  })
+
+  vim.keymap.set("n", "<leader>b", require("dap").toggle_breakpoint)
+  vim.keymap.set("n", "<leader>c", require("dap").continue)
+  vim.keymap.set("n", "<leader>s", require("dap").step_into)
+  vim.keymap.set("n", "<leader>n", require("dap").step_over)
+  vim.keymap.set("n", "<leader>o", require("dap").step_out)
+  vim.keymap.set("n", "<leader>r", require("dap").repl.toggle)
+  vim.keymap.set("n", "<leader>u", require("dapui").toggle)
+end
+
+return export
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -2,99 +2,103 @@
 local export = {}
 
 function export.config()
-  local lspconfig = require("lspconfig")
+	local lspconfig = require("lspconfig")
 
-  -- share capabilities of lsp with lsp clients
-  local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
+	-- share capabilities of lsp with lsp clients
+	local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
-  local on_format = function()
-    vim.lsp.buf.format({ async = true })
-  end
+	local on_format = function()
+		vim.lsp.buf.format({ async = true })
+	end
 
-  -- local on_attach = function(client, bufnr)
-  local on_attach = function()
-    vim.keymap.set("n", "gh", vim.lsp.buf.hover, { buffer = 0 }) -- hover information
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = 0 }) -- go to definition of function
-    vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, { buffer = 0 }) -- go to definition of a type
-    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { buffer = 0 }) -- go to implementaion of an interface or method
-    vim.keymap.set("n", "<leader>fd", require("telescope.builtin").diagnostics, { buffer = 0 }) -- list diagnostics
-    vim.keymap.set("n", "<leader>fr", require("telescope.builtin").lsp_references, { buffer = 0 }) -- show refrences hight lighted word
-    vim.keymap.set("n", "gf", on_format, { buffer = 0 }) -- format
-    vim.keymap.set("n", "ge", vim.diagnostic.open_float) -- explain errors
-  end
+	local on_attach = function()
+		vim.keymap.set("n", "gh", vim.lsp.buf.hover, { buffer = 0 }) -- hover information
+		vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = 0 }) -- go to definition of function
+		vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, { buffer = 0 }) -- go to definition of a type
+		vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { buffer = 0 }) -- go to implementaion of an interface or method
+		vim.keymap.set("n", "<leader>fd", require("telescope.builtin").diagnostics, { buffer = 0 }) -- list diagnostics
+		vim.keymap.set("n", "<leader>fr", require("telescope.builtin").lsp_references, { buffer = 0 }) -- show refrences hight lighted word
+		vim.keymap.set("n", "gf", on_format, { buffer = 0 }) -- format
+		vim.keymap.set("n", "ge", vim.diagnostic.open_float, { buffer = 0 }) -- explain errors
+	end
 
-  -- lua
-  lspconfig.sumneko_lua.setup({
-    on_attach = on_attach,
-    capabilities = capabilities,
-  })
+	-- lua
+	lspconfig.sumneko_lua.setup({
+		on_attach = on_attach,
+		capabilities = capabilities,
+	})
 
-  -- golang
-  lspconfig.gopls.setup({
-    on_attach = on_attach,
-    capabilities = capabilities,
-  })
+	-- golang
+	lspconfig.gopls.setup({
+		on_attach = on_attach,
+		capabilities = capabilities,
+		root_dir = lspconfig.util.root_pattern("go.mod"),
+	})
 
-  -- rust
-  lspconfig.rust_analyzer.setup({
-    on_attach = on_attach,
-    capabilities = capabilities,
-  })
+	-- rust
+	lspconfig.rust_analyzer.setup({
+		on_attach = on_attach,
+		capabilities = capabilities,
+		root_dir = lspconfig.util.root_pattern("Cargo.toml"),
+	})
 
-  -- c/cpp
-  lspconfig.clangd.setup({
-    on_attach = on_attach,
-    capabilities = capabilities,
-  })
+	-- c/cpp
+	lspconfig.clangd.setup({
+		on_attach = on_attach,
+		capabilities = capabilities,
+		root_dir = lspconfig.util.root_pattern("Makefile"),
+	})
 
-  -- python
-  lspconfig.pylsp.setup({
-    on_attach = on_attach,
-    capabilities = capabilities,
-  })
+	-- python
+	lspconfig.pylsp.setup({
+		on_attach = on_attach,
+		capabilities = capabilities,
+	})
 
-  -- deno
-  lspconfig.denols.setup({
-    on_attach = on_attach,
-    capabilities = capabilities,
-  })
+	-- deno
+	lspconfig.denols.setup({
+		on_attach = on_attach,
+		capabilities = capabilities,
+		root_dir = lspconfig.util.root_pattern("deno.json"),
+	})
 
-  -- ruby
-  lspconfig.solargraph.setup({
-    on_attach = on_attach,
-    capabilities = capabilities,
-  })
+	-- ruby
+	lspconfig.solargraph.setup({
+		on_attach = on_attach,
+		capabilities = capabilities,
+		root_dir = lspconfig.util.root_pattern("Gemfile"),
+	})
 
-  -- format on save
-  vim.api.nvim_create_autocmd("BufWritePre", {
-    pattern = "*.py",
-    callback = on_format,
-  })
+	-- format on save
+	vim.api.nvim_create_autocmd("BufWritePre", {
+		pattern = "*.py",
+		callback = on_format,
+	})
 
-  vim.api.nvim_create_autocmd("BufWritePre", {
-    pattern = "*.rs",
-    callback = on_format,
-  })
+	vim.api.nvim_create_autocmd("BufWritePre", {
+		pattern = "*.rs",
+		callback = on_format,
+	})
 
-  vim.api.nvim_create_autocmd("BufWritePre", {
-    pattern = "*.c",
-    callback = on_format,
-  })
+	-- vim.api.nvim_create_autocmd("BufWritePre", {
+	--   pattern = "*.c",
+	--   callback = on_format,
+	-- })
 
-  vim.api.nvim_create_autocmd("BufWritePre", {
-    pattern = "*.cpp",
-    callback = on_format,
-  })
+	-- vim.api.nvim_create_autocmd("BufWritePre", {
+	--   pattern = "*.cpp",
+	--   callback = on_format,
+	-- })
 
-  vim.api.nvim_create_autocmd("BufWritePre", {
-    pattern = "*.go",
-    callback = on_format,
-  })
+	vim.api.nvim_create_autocmd("BufWritePre", {
+		pattern = "*.go",
+		callback = on_format,
+	})
 
-  vim.api.nvim_create_autocmd("BufWritePre", {
-    pattern = "*.lua",
-    callback = on_format,
-  })
+	-- vim.api.nvim_create_autocmd("BufWritePre", {
+	--   pattern = "*.lua",
+	--   callback = on_format,
+	-- })
 end
 
 return export
