660adae3d1c45434482dbe4f70f5079b322607dd
ruahman <ruahman@pop-os.localdomain>
2022-12-06 20:24:50 -0400

    cleaned up hop

--- lua/plugins/hop.lua
+++ lua/plugins/hop.lua
@@ -6,9 +6,8 @@ function export.config()
   local keymap = vim.api.nvim_set_keymap
   local opts = { noremap = true, silent = true }
 
-  keymap('n','<Leader>w',"<cmd>lua require'hop'.hint_words()<cr>", opts)
-  -- keymap('n','<Leader>f',"<cmd>lua require'hop'.hint_char1()<cr>", opts)
   keymap('n','<Leader>s',"<cmd>lua require'hop'.hint_char2()<cr>", opts)
+  keymap('n','<Leader>w',"<cmd>lua require'hop'.hint_words()<cr>", opts)
 end
 
 return export
