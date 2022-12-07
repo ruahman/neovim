39bc7f037994d418ddfdeda671ae0597106e55d3
ruahman <ruahman@pop-os.localdomain>
2022-12-06 20:19:26 -0400

    added snippets

--- lua/snippets/rust.lua
+++ lua/snippets/rust.lua
@@ -1,2 +1,26 @@
 return {
+    s("tests",fmt([[
+#[cfg(test)]
+mod {} {{
+    use super::*;
+    #[test]
+    fn {}() {{
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }}
+}}
+    ]],{
+      i(1,"tests"),
+      i(2,"test")
+    })),
+    s("struct derive(Debug) allow(dead_code)",fmt([[
+#[allow(dead_code)]
+#[derive(Debug)]
+struct {} {{
+  {}
+}} 
+    ]],{
+      i(1,"name"),
+      i(2,"field")
+    })),
 }
