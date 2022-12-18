4619a68ab60196e7a16eca39a3a8c1cb396e81ad
Diego Vila <diego@velascommerce.com>
2022-12-17 22:43:15 -0400

    made some changes to nvim-tree

--- lua/keys/init.lua
+++ lua/keys/init.lua
@@ -1,22 +1,18 @@
-local keymap = vim.api.nvim_set_keymap
-local opts = { noremap = true, silent = true }
+local map = require('utils').map
 
 -- set leader key
 vim.g.mapleader = ' '
 
-keymap('n', '<Leader>h', ':set hlsearch!<CR>', opts)
-keymap('n', '<Leader>l', ':set list!<CR>', opts)
+map('n', '<Leader>h', ':set hlsearch!<CR>')
+map('n', '<Leader>l', ':set list!<CR>')
 
--- keymap('i', 'jk', '<ESC>', opts)
--- keymap('i', 'kj', '<ESC>', opts)
+map('v', '<', '<gv')
+map('v', '>', '>gv')
 
-keymap('v', '<', '<gv', opts)
-keymap('v', '>', '>gv', opts)
-
-keymap('n', '<C-h>', '<C-w>h', opts)
-keymap('n', '<C-j>', '<C-w>j', opts)
-keymap('n', '<C-k>', '<C-w>k', opts)
-keymap('n', '<C-l>', '<C-w>l', opts)
+map('n', '<C-h>', '<C-w>h')
+map('n', '<C-j>', '<C-w>j')
+map('n', '<C-k>', '<C-w>k')
+map('n', '<C-l>', '<C-w>l')
 
 -- keymap('n', '<M-h>', ':vertical resize +2<CR>', opts)
 -- keymap('n', '<M-l>', ':vertical resize -2<CR>', opts)
@@ -24,15 +20,10 @@ keymap('n', '<C-l>', '<C-w>l', opts)
 -- utils.map('n', '<M-k>', ':resize +2<CR>')
 
 -- move line
-keymap('n', '<M-j>', ':m .+1<cr>==', opts)
-keymap('n', '<M-k>', ':m .-2<CR>==', opts)
-keymap('v', '<M-j>', ":m '>+1<cr>gv=gv", opts)
-keymap('v', '<M-k>', ":m '<-2<cr>gv=gv", opts)
+map('n', '<M-j>', ':m .+1<cr>==')
+map('n', '<M-k>', ':m .-2<CR>==')
+map('v', '<M-j>', ":m '>+1<cr>gv=gv")
+map('v', '<M-k>', ":m '<-2<cr>gv=gv")
 
--- move through buffers
-keymap('n', '<C-f>', ':bnext<CR>', opts)
-
--- save shortcut
--- keymap('n', '<C-s>', ':w<CR>', opts)
-
--- vim.cmd[[command! -nargs=+ Go :! go <args> .]]
+-- cd
+map('n', 'cd', ':cd %:p:h<CR>')
--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,22 +1,20 @@
 local export = {}
 
 function export.config()
+  local map = require('utils').map
+
   -- disable netrw at the very start of your init.lua (strongly advised)
   vim.g.loaded_netrw = 1
   vim.g.loaded_netrwPlugin = 1
 
-  -- set termguicolors to enable highlight groups
-  vim.opt.termguicolors = true
-
   -- empty setup using defaults
-  require'nvim-tree'.setup{}
+  require'nvim-tree'.setup{
+    auto_reload_on_write = true,
+    sync_root_with_cwd = true,
+  }
 
   -- setup keymap
-  local keymap = vim.api.nvim_set_keymap
-  local opts = { noremap = true, silent = true }
-  keymap('n', '<Leader>e', ':NvimTreeToggle<CR>', opts)
-
-  -- vim.g.nvim_tree_quit_on_open = 1
+  map('n', '<Leader>e', ':NvimTreeToggle<CR>')
 end
 
 return export
