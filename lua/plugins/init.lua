02bcc8b6e9e1c038ed94440a972f7e798b962079
ruahman <dego_vila@yahoo.com>
2023-02-16 12:43:16 -0400

    fixed telescope for windows

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -32,7 +32,6 @@ return require("packer").startup(function(use)
 
   use("wellle/targets.vim")
 
-  -- use("tpope/vim-commentary")
   use({
     "numToStr/Comment.nvim",
     config = require("plugins.comment").config
@@ -43,8 +42,8 @@ return require("packer").startup(function(use)
   use("tpope/vim-fugitive")
 
   use({
-    "lewis6991/gitsigns.nvim",
-    config = require("plugins.gitsigns").config
+   "lewis6991/gitsigns.nvim",
+   config = require("plugins.gitsigns").config
   })
 
   use("romainl/vim-cool")
@@ -54,64 +53,64 @@ return require("packer").startup(function(use)
   use("moll/vim-bbye")
 
   use({
-    "lukas-reineke/indent-blankline.nvim",
-    config = require("plugins.indent-blankline").config
+   "lukas-reineke/indent-blankline.nvim",
+   config = require("plugins.indent-blankline").config
   })
 
   -- hop
   use({
-    "phaazon/hop.nvim",
-    config = require("plugins.hop").config,
+   "phaazon/hop.nvim",
+   config = require("plugins.hop").config,
   })
 
   -- quick-scope
   use({
-    "unblevable/quick-scope",
-    config = require("plugins.quick-scope").config,
+   "unblevable/quick-scope",
+   config = require("plugins.quick-scope").config,
   })
 
   -- nvim-tree
   use({
-    "kyazdani42/nvim-tree.lua",
-    requires = {
-      "kyazdani42/nvim-web-devicons",
-    },
-    config = require("plugins.nvim-tree").config,
+   "kyazdani42/nvim-tree.lua",
+   requires = {
+     "kyazdani42/nvim-web-devicons",
+   },
+   config = require("plugins.nvim-tree").config,
   })
 
   -- lualine
   use({
-    "hoob3rt/lualine.nvim",
-    config = require("plugins.lualine").config,
+   "hoob3rt/lualine.nvim",
+   config = require("plugins.lualine").config,
   })
 
   -- bufferline
   use({
-    'akinsho/bufferline.nvim',
-    tag = "v2.*",
-    requires = {
-      'kyazdani42/nvim-web-devicons',
-      'famiu/bufdelete.nvim'
-    },
-    config = require('plugins.bufferline').config
+   'akinsho/bufferline.nvim',
+   tag = "v2.*",
+   requires = {
+     'kyazdani42/nvim-web-devicons',
+     'famiu/bufdelete.nvim'
+   },
+   config = require('plugins.bufferline').config
   })
 
   -- telescope
   use({
-    "nvim-telescope/telescope.nvim",
-    requires = {
-      "nvim-lua/plenary.nvim",
-      "nvim-lua/popup.nvim",
-    },
-    config = require("plugins.telescope").config,
+   "nvim-telescope/telescope.nvim",
+   requires = {
+     "nvim-lua/plenary.nvim",
+     "nvim-lua/popup.nvim",
+   },
+   config = require("plugins.telescope").config,
   })
-  use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
+  -- use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
 
   -- snippets
   use("L3MON4D3/LuaSnip")
   use({
-    "rafamadriz/friendly-snippets",
-    config = require("plugins.luasnip").config,
+   "rafamadriz/friendly-snippets",
+   config = require("plugins.luasnip").config,
   })
 
   -- completion
@@ -122,14 +121,14 @@ return require("packer").startup(function(use)
   use("saadparwaiz1/cmp_luasnip")
   use("onsails/lspkind.nvim")
   use({
-    "hrsh7th/nvim-cmp",
-    config = require("plugins.nvim-cmp").config,
+   "hrsh7th/nvim-cmp",
+   config = require("plugins.nvim-cmp").config,
   })
 
   -- lsp
   use({
-    "neovim/nvim-lspconfig",
-    config = require("plugins.lsp").config,
+   "neovim/nvim-lspconfig",
+   config = require("plugins.lsp").config,
   })
 
   -- dap
@@ -138,54 +137,54 @@ return require("packer").startup(function(use)
   use("mfussenegger/nvim-dap-python")
   use("theHamsta/nvim-dap-virtual-text")
   use({
-    "rcarriga/nvim-dap-ui",
-    config = require("plugins.nvim-dap").config,
+   "rcarriga/nvim-dap-ui",
+   config = require("plugins.nvim-dap").config,
   })
 
 
   -- mason
   use("williamboman/mason.nvim")
   use({
-    "williamboman/mason-lspconfig.nvim",
-    config = require("plugins.mason").config,
+   "williamboman/mason-lspconfig.nvim",
+   config = require("plugins.mason").config,
   })
 
   -- trouble
   use({
-    "folke/trouble.nvim",
-    requires = "kyazdani42/nvim-web-devicons",
-    config = require("plugins.trouble").config,
+   "folke/trouble.nvim",
+   requires = "kyazdani42/nvim-web-devicons",
+   config = require("plugins.trouble").config,
   })
 
   -- terminal
   use({
-    "akinsho/toggleterm.nvim",
-    tag = "v2.*",
-    config = require("plugins.toogleterm").config,
+   "akinsho/toggleterm.nvim",
+   tag = "v2.*",
+   config = require("plugins.toogleterm").config,
   })
 
   -- dashboard
   use({
-    "goolord/alpha-nvim",
-    config = require("plugins.dashboard").config,
+   "goolord/alpha-nvim",
+   config = require("plugins.dashboard").config,
   })
 
   use({
-    'vimwiki/vimwiki',
-    config = require('plugins.vimwiki').config
+   'vimwiki/vimwiki',
+   config = require('plugins.vimwiki').config
   })
 
   use({
-    "nvim-neotest/neotest",
-    requires = {
-      "nvim-lua/plenary.nvim",
-      "nvim-treesitter/nvim-treesitter",
-      "antoinemadec/FixCursorHold.nvim",
-      "Issafalcon/neotest-dotnet",
-      "nvim-neotest/neotest-python",
-      "nvim-neotest/neotest-go"
-    },
-    config = require('plugins.neotest').config
+   "nvim-neotest/neotest",
+   requires = {
+     "nvim-lua/plenary.nvim",
+     "nvim-treesitter/nvim-treesitter",
+     "antoinemadec/FixCursorHold.nvim",
+     "Issafalcon/neotest-dotnet",
+     "nvim-neotest/neotest-python",
+     "nvim-neotest/neotest-go"
+   },
+   config = require('plugins.neotest').config
   })
 
   -- Put this at the end after all plugins
