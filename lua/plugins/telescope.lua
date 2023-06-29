df32cd0b8b1b9843fb8169fefc559c55d92fb3cd
ruahman <dego_vila@yahoo.com>
2023-06-29 00:09:50 -0400

    addded git

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,28 +1,29 @@
 local export = {}
 
 function export.config()
-  require("telescope").setup({
-    defaults = {
-      mappings = {
-        n = {
-          ["bd"] = require("telescope.actions").delete_buffer,
-        },
-        i = {
-          ["bd"] = require("telescope.actions").delete_buffer,
-        },
-      },
-    },
-  })
+	require("telescope").setup({
+		defaults = {
+			mappings = {
+				n = {
+					["bd"] = require("telescope.actions").delete_buffer,
+				},
+				i = {
+					["bd"] = require("telescope.actions").delete_buffer,
+				},
+			},
+		},
+	})
 
-  -- load faster fuzy finder
-  require("telescope").load_extension("fzf")
+	-- load faster fuzy finder
+	require("telescope").load_extension("fzf")
 
-  local builtin = require("telescope.builtin")
+	local builtin = require("telescope.builtin")
 
-  vim.keymap.set("n", "<leader>ff", builtin.find_files)               -- list files
-  vim.keymap.set("n", "<leader>fb", builtin.buffers)                  -- list buffers
-  vim.keymap.set("n", "<leader>fs", builtin.current_buffer_fuzzy_find) -- search in buffer
-  vim.keymap.set("n", "<leader>fg", builtin.live_grep)                -- grep in dir
+	vim.keymap.set("n", "<leader>ff", builtin.find_files) -- list files
+	vim.keymap.set("n", "<leader>fb", builtin.buffers) -- list buffers
+	vim.keymap.set("n", "<leader>fs", builtin.current_buffer_fuzzy_find) -- search in buffer
+	vim.keymap.set("n", "<leader>fg", builtin.live_grep) -- grep in dir
+	vim.keymap.set("n", "<leader>g", builtin.git_status) -- git status
 end
 
 return export
