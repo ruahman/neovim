0e11c9dac88f40c6d4e6d92410bc3fe68125f782
diegovelascommerce <diego@velascommerce.com>
2023-09-29 19:52:31 -0400

    got ominsharp working again becausel csharp-lsp is not good

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -108,12 +108,6 @@ function M.config()
 		on_attach = on_attach,
 	})
 
-	-- csharp
-	lspconfig.csharp_ls.setup({
-		capabilities = capabilities,
-		on_attach = on_attach,
-	})
-
 	-- c/cpp
 	lspconfig.clangd.setup({
 		on_attach = on_attach,
@@ -124,6 +118,98 @@ function M.config()
 	lspconfig.zls.setup({
 		on_attach = on_attach,
 		capabilities = capabilities,
+	})
+
+	-- csharp
+	lspconfig.omnisharp.setup({
+		cmd = { vim.fn.stdpath("data") .. "/mason/bin/omnisharp" },
+		-- Enables support for reading code style, naming convention and analyzer
+		-- settings from .editorconfig.
+		enable_editorconfig_support = true,
+		-- Specifies whether 'using' directives should be grouped and sorted during
+		-- document formatting.
+		organize_imports_on_format = false,
+		on_attach = function(client, bufnr)
+			on_attach(client, bufnr)
+
+			-- Semantic tokens do not conform to the LSP specification
+			-- https://github.com/OmniSharp/omnisharp-roslyn/issues/2483
+			-- https://github.com/neovim/neovim/issues/21391
+			client.server_capabilities.semanticTokensProvider = {
+				full = vim.empty_dict(),
+				legend = {
+					tokenModifiers = { "static_symbol" },
+					tokenTypes = {
+						"comment",
+						"excluded_code",
+						"identifier",
+						"keyword",
+						"keyword_control",
+						"number",
+						"operator",
+						"operator_overloaded",
+						"preprocessor_keyword",
+						"string",
+						"whitespace",
+						"text",
+						"static_symbol",
+						"preprocessor_text",
+						"punctuation",
+						"string_verbatim",
+						"string_escape_character",
+						"class_name",
+						"delegate_name",
+						"enum_name",
+						"interface_name",
+						"module_name",
+						"struct_name",
+						"type_parameter_name",
+						"field_name",
+						"enum_member_name",
+						"constant_name",
+						"local_name",
+						"parameter_name",
+						"method_name",
+						"extension_method_name",
+						"property_name",
+						"event_name",
+						"namespace_name",
+						"label_name",
+						"xml_doc_comment_attribute_name",
+						"xml_doc_comment_attribute_quotes",
+						"xml_doc_comment_attribute_value",
+						"xml_doc_comment_cdata_section",
+						"xml_doc_comment_comment",
+						"xml_doc_comment_delimiter",
+						"xml_doc_comment_entity_reference",
+						"xml_doc_comment_name",
+						"xml_doc_comment_processing_instruction",
+						"xml_doc_comment_text",
+						"xml_literal_attribute_name",
+						"xml_literal_attribute_quotes",
+						"xml_literal_attribute_value",
+						"xml_literal_cdata_section",
+						"xml_literal_comment",
+						"xml_literal_delimiter",
+						"xml_literal_embedded_expression",
+						"xml_literal_entity_reference",
+						"xml_literal_name",
+						"xml_literal_processing_instruction",
+						"xml_literal_text",
+						"regex_comment",
+						"regex_character_class",
+						"regex_anchor",
+						"regex_quantifier",
+						"regex_grouping",
+						"regex_alternation",
+						"regex_text",
+						"regex_self_escaped_character",
+						"regex_other_escape",
+					},
+				},
+				range = true,
+			}
+		end,
 	})
 end
 
