cf19e8798ae2ff80796ee0784f1b2418849b098b
ruahman <dego_vila@yahoo.com>
2023-09-28 20:48:31 -0400

    close left and right buffer line

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,29 +1,30 @@
-
 local M = {}
 
 function M.config()
-  local map = require('utils').map
+	local map = require("utils").map
 
-  require('bufferline').setup {
-    options = {
-      -- close_command = "Bdelete! %d",
-      offsets = {
-        {
-          filetype = "NvimTree",
-          text = "nvim-tree",
-          highlight = "Directory",
-          text_align = "left"
-        }
-      },
-      diagnostics = "nvim_lsp",
-    }
-  }
+	require("bufferline").setup({
+		options = {
+			-- close_command = "Bdelete! %d",
+			offsets = {
+				{
+					filetype = "NvimTree",
+					text = "nvim-tree",
+					highlight = "Directory",
+					text_align = "left",
+				},
+			},
+			diagnostics = "nvim_lsp",
+		},
+	})
 
-  map('n','<S-l>',':BufferLineCycleNext<CR>')
+	map("n", "<S-l>", ":BufferLineCycleNext<CR>")
+	map("n", "<S-right>", ":BufferLineCloseRight<CR>")
 
-  map('n', '<S-h>',':BufferLineCyclePrev<CR>')
+	map("n", "<S-h>", ":BufferLineCyclePrev<CR>")
+	map("n", "<S-left>", ":BufferLineCloseLeft<CR>")
 
-  map('n', 'bd',':Bdelete<CR>')
+	map("n", "bd", ":Bdelete<CR>")
 end
 
 return M
