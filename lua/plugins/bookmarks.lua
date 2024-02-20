bc9e80951e401d3b1bad5f0b9e7d02404081edd0
Diego Vila <diego@velascommerce.com>
2024-02-19 20:29:03 -0400

    book marks

--- lua/plugins/bookmarks.lua
+++ lua/plugins/bookmarks.lua
@@ -4,12 +4,12 @@ return {
 		sign_priority = 8, --set bookmark sign priority to cover other sign
 		save_file = vim.fn.expand("$HOME/.bookmarks"), -- bookmarks save file path
 		keywords = {
+			["@m"] = "󰃃",
+			["@f"] = "󰉀",
+			["@h"] = "󰣐",
 			["@t"] = "",
 			["@w"] = "",
 			["@x"] = "󱁤",
-			["@n"] = "󰎈",
-			["@h"] = "󰣐",
-			["@f"] = "󰉀",
 		},
 		on_attach = function(bufnr)
 			local bm = require("bookmarks")
@@ -17,13 +17,13 @@ return {
 			require("telescope").load_extension("bookmarks")
 
 			local map = vim.keymap.set
-			map("n", "mm", bm.bookmark_toggle) -- add or remove bookmark at current line
-			map("n", "mi", bm.bookmark_ann) -- add or edit mark annotation at current line
-			map("n", "mc", bm.bookmark_clean) -- clean all marks in local buffer
-			map("n", "mn", bm.bookmark_next) -- jump to next mark in local buffer
-			map("n", "mp", bm.bookmark_prev) -- jump to previous mark in local buffer
+			map("n", "mt", bm.bookmark_toggle) -- add or remove bookmark at current line
+			map("n", "mm", bm.bookmark_ann) -- add or edit mark annotation at current line
+			map("n", "ml", bookmarks.list) -- marks
+			-- map("n", "mc", bm.bookmark_clean) -- clean all marks in local buffer
+			-- map("n", "mn", bm.bookmark_next) -- jump to next mark in local buffer
+			-- map("n", "mp", bm.bookmark_prev) -- jump to previous mark in local buffer
 			-- map("n", "ml", bm.bookmark_list) -- show marked file list in quickfix window
-			map("n", "<leader>ml", bookmarks.list) -- marks
 		end,
 	},
 	config = true,
