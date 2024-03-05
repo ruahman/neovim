ae67aed7d3b9d3240b97e8539a37aff589ff524c
Diego Vila <diego@velascommerce.com>
2024-03-05 17:12:50 -0400

    fix telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -17,14 +17,17 @@ local function config()
 
 	local builtin = require("telescope.builtin")
 
-	vim.keymap.set("n", "<leader>ff", builtin.find_files) -- list files
-	vim.keymap.set("n", "<leader>fb", builtin.buffers) -- list buffers
-	vim.keymap.set("n", "<leader>fs", builtin.current_buffer_fuzzy_find) -- search in buffer
-	vim.keymap.set("n", "<leader>fg", builtin.live_grep) -- grep in dir
-	vim.keymap.set("n", "<leader>gt", builtin.git_status) -- git status
-	vim.keymap.set("n", "<leader>cm", builtin.commands) -- nvim commands
-	vim.keymap.set("n", "<leader>jm", builtin.jumplist) -- jumplist
-	vim.keymap.set("n", "<leader>mk", builtin.marks) -- marks
+	vim.keymap.set("n", "<leader>f", function()
+		builtin.find_files({ hidden = true, no_ignore = true })
+	end) -- list files
+
+	vim.keymap.set("n", "<leader>b", builtin.buffers) -- list buffers
+	vim.keymap.set("n", "<leader>s", builtin.current_buffer_fuzzy_find) -- search in buffer
+	vim.keymap.set("n", "<leader>r", builtin.live_grep) -- grep in dir
+	vim.keymap.set("n", "<leader>g", builtin.git_status) -- git status
+	vim.keymap.set("n", "<leader>j", builtin.jumplist) -- jumplist
+	-- vim.keymap.set("n", "<leader>cm", builtin.commands) -- nvim commands
+	-- vim.keymap.set("n", "<leader>mk", builtin.marks) -- marks
 end
 
 return {
