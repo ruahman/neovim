c6cbcd24e3ea1104c4dacd13e33aec708f620451
ruahman <dego_vila@yahoo.com>
2024-07-04 15:16:39 -0400

    commit

--- lua/plugins/marks.lua
+++ lua/plugins/marks.lua
@@ -1,10 +1,10 @@
 -- A better user experience for interacting with and manipulating Vim marks.
 return {
+	enabled = true,
 	"chentoast/marks.nvim",
 	event = "VeryLazy",
 	opts = {
 		force_write_shada = true,
 	},
 	config = true,
-	enabled = false,
 }
