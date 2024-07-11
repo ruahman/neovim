e6dd79d8bb384502b66354a79586bbbce55ed4f8
Diego Vila <diego@velascommerce.com>
2024-07-11 18:00:37 -0400

    snippets
    fixed some snippets

--- lua/snippets/rust.lua
+++ lua/snippets/rust.lua
@@ -1,26 +1,21 @@
 return {
-    s("tests",fmt([[
+	s(
+		"tests",
+		fmt(
+			[[
 #[cfg(test)]
-mod {} {{
+mod tests {{
     use super::*;
     #[test]
-    fn {}() {{
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn test_{}() {{
+        {}
     }}
 }}
-    ]],{
-      i(1,"tests"),
-      i(2,"test")
-    })),
-    s("struct derive(Debug) allow(dead_code)",fmt([[
-#[allow(dead_code)]
-#[derive(Debug)]
-struct {} {{
-  {}
-}} 
-    ]],{
-      i(1,"name"),
-      i(2,"field")
-    })),
+    ]],
+			{
+				i(1, "method"),
+				i(0),
+			}
+		)
+	),
 }
