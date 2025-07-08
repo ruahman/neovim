674d1cfca4fe3cbc9be799c8d151a803fef9e4bd
Diego R Vila <dego_vila@yahoo.com>
2025-07-08 15:46:24 -0400

    clean it up a little

--- lua/config/commands.lua
+++ lua/config/commands.lua
@@ -1,20 +1,3 @@
--- Make commands
--- vim.api.nvim_create_user_command(
---   'Make',
---   function(input)
---     if string.match(input.args, "build") then
---       vim.cmd[[:! make build]]
---     elseif string.match(input.args, "clean") then
---       vim.cmd[[:! make clean]]
---     elseif string.match(input.args, "run") then
---       vim.cmd[[:! make run]]
---     else
---       vim.cmd[[:! make]]
---     end
---   end,
---   {nargs = '?'}
--- )
-
 --Scratch
 vim.api.nvim_create_user_command("Scratch", function()
 	vim.api.nvim_command("enew")
