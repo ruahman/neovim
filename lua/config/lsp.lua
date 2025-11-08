587fd9e2fdafd835cd0a556b2a1b90bb8d4536e1
ruahman <ruahman@gmail.com>
2025-11-08 04:13:01 +0000

    take out a comment

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,15 +1,3 @@
--- this gets setup in lsp-config
--- vim.lsp.config("lua_ls", {
--- 	cmd = { "lua-language-server" },
--- 	filetypes = { "lua" },
--- 	root_markers = {
--- 		".luarc.json",
--- 		".luacheckrc",
--- 		".git",
--- 	},
--- })
-
--- :checkhealth vim.lsp
 vim.lsp.enable("lua_ls")
 vim.lsp.enable("ts_ls")
 vim.lsp.enable("pyright")
