57675572943ed36b7b7f256fa42f7169df7528e4
ruahman <ruahman@gmail.com>
2026-03-21 12:43:48 +0000

    update snacks

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -6,12 +6,9 @@ return {
 		dashboard = {
 			preset = {
 				header = [[
- NEOVIM
+NEOVIM
         ]],
 				keys = {
-					{ icon = " ", key = "e", desc = "New File", action = ":ene | startinsert" },
-					{ icon = " ", key = "f", desc = "Find File", action = ":Telescope find_files" },
-					{ icon = " ", key = "r", desc = "Recent Files", action = ":Telescope oldfiles" },
 					{ icon = " ", key = "s", desc = "Settings", action = ":e $MYVIMRC | cd %:p:h" },
 					{ icon = " ", key = "q", desc = "Quit", action = ":qa" },
 				},
