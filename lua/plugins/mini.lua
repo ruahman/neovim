95f6b07a7ad5ae454b6a8eaa54f181c0c1c5d590
ruahman <ruahman@gmail.com>
2026-03-22 13:37:05 +0000

    update

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -2,7 +2,7 @@ return {
 	"echasnovski/mini.nvim",
 	version = "*",
 	config = function()
-		require("mini.ai").setup()
+		require("mini.ai").setup() -- text objects
 		require("mini.surround").setup()
 		require("mini.pairs").setup()
 		require("mini.comment").setup()
